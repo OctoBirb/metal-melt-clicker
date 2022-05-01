da2ec931e1cb6746c9b98641a394c5432caf405c
OctoBirb <uncledj71@gmail.com>
2022-04-30 20:44:33 -0400

    a lot

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+var update=(x,y)=>{document.getElementById(x).innerText=y}
+
 var joules = 0
 var watts  = 0
+
+var coalgen = {
+    cost     : 0
+    ,time    : 0
+    ,max     : 100
+    ,quandale: 0
+    ,markiply: 1
+}
+
+function EarnJ(x) {
+    joules += x
+}
+
+window.setInterval(function() {
+    update()
+}, 33)
